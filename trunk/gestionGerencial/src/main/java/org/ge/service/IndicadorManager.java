0c273afacaf4100a739881ac93104d9cffc1c2da
mmerinovega@gmail.com <mmerinovega@gmail.com@e721ef72-feba-3062-5dd9-1d49d08ceec4>
2013-05-20 23:59:59 +0000

    V5.1

--- trunk/gestionGerencial/src/main/java/org/ge/service/IndicadorManager.java
+++ trunk/gestionGerencial/src/main/java/org/ge/service/IndicadorManager.java
@@ -3,6 +3,7 @@ package org.ge.service;
 import java.util.List;
 
 import org.ge.model.Indicador;
+import org.ge.util.IndicadorUtil;
 
 public interface IndicadorManager {
 
@@ -11,8 +12,12 @@ public interface IndicadorManager {
 	public List<Indicador> getListaIndicadores(Indicador indicador);
 
 	public Indicador getIndicadorPorCodigo(Integer codigoIndicador);
+	
+	public IndicadorUtil getIndicadorUtilPorCodigo(Integer codigoIndicador);
 
 	public void guardarIndicador(Indicador indicador);
 	
 	public List<Indicador> getListaIndicadoresPendientes();
+	
+	public boolean isRepeatNombre(String descripcion,Integer codigoIndicador);
 }
--- trunk/gestionGerencial/src/main/java/org/ge/service/impl/IndicadorManagerImpl.java
+++ trunk/gestionGerencial/src/main/java/org/ge/service/impl/IndicadorManagerImpl.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.ge.dao.IndicadorDao;
 import org.ge.model.Indicador;
+import org.ge.model.ObjetivoFuncional;
 import org.ge.service.IndicadorManager;
+import org.ge.util.IndicadorUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +28,24 @@ class IndicadorManagerImpl implements IndicadorManager {
 	public Indicador getIndicadorPorCodigo(Integer codigoIndicador) {
 		return indicadorDao.getIndicadorPorCodigo(codigoIndicador);
 	}
+	
+	public IndicadorUtil getIndicadorUtilPorCodigo(Integer codigoIndicador) {
+		Indicador indicador = indicadorDao.getIndicadorPorCodigo(codigoIndicador);
+		IndicadorUtil aux = new IndicadorUtil();
+		aux.setCodigo(indicador.getCodigo());
+		aux.setComentarios(indicador.getComentarios());
+		aux.setDescripcion(indicador.getDescripcion());
+		aux.setObjetivo(indicador.getObjetivo());
+		aux.setEstado(indicador.getEstado());
+		aux.setFechaRegistro(indicador.getFechaRegistro());
+		aux.setFormula(indicador.getFormula());
+		aux.setMeta(indicador.getMeta());
+		aux.setNombre(indicador.getNombre());
+		if(indicador.getObjetivo().getTipoObjetivo().equals("Funcional")){
+			aux.setArea(((ObjetivoFuncional)indicador.getObjetivo()).getArea());
+		}
+		return aux;
+	}
 
 	public void guardarIndicador(Indicador indicador) {
 		indicadorDao.guardarIndicador(indicador);
@@ -36,6 +56,11 @@ class IndicadorManagerImpl implements IndicadorManager {
 		lista.addAll(getListaIndicadores("pendUpdate"));
 		lista.addAll(getListaIndicadores("pendBaja"));
 		return lista;		
+	}
+	
+	public boolean isRepeatNombre(String nombre,
+			Integer codigoIndicador) {
+		return indicadorDao.isRepeatDescription(nombre, codigoIndicador);
 	}
 
 }
